cd9e1e422f6d605d30d6ee702f50f3f3b8df0f64
金林成宇 <jinlinchengyu@meituan.com>
2019-04-28 19:57:04 +0800

    webpack config rules change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
-import App from './app/App/index'
-import Home from './app/Home'
+import App from './src/App/index'
+import Home from './src/Home'
 import './index.scss'
 
 ReactDOM.render((
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,30 @@
 module.exports = {
-    entry: './index.js',
-    devtool: 'cheap-source-map',
-    output: {
-      filename: 'dist/bundle.js',
-      publicPath: ''
-    }, node: {
-        fs: 'empty'
-    },
-    module: {
-      loaders: [
-        {
-            test: /(\.jsx|\.js)$/,
-            use: {
-                loader: "babel-loader",
-                //以下内容已存放于.babelrc中
-                options: {
-                    presets: [
-                        "env", "react"
-                    ]
-                }
-            },
-            exclude: /node_modules/
+  entry: './index.js',
+  devtool: 'cheap-source-map',
+  output: {
+    filename: 'dist/bundle.js',
+    publicPath: ''
+  },
+  module: {
+    rules: [{
+        test: /(\.jsx|\.js)$/,
+        loader: "babel-loader",
+        //以下内容已存放于.babelrc中
+        options: {
+          presets: [
+            "env", "react"
+          ]
         },
-        {
-            test: /\.scss$/,
-            use: ['style-loader','css-loader','sass-loader'],
-        },
-        {
-            test: /\.(jpg|png|jpeg|gif)$/,
-            use: ['url-loader'],
-        }
-      ]
-    }
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/,
+        loader: ['url-loader'],
+      }
+    ]
   }
-  
+}
